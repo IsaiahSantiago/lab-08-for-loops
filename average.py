2c1dfa5e4d990a2e14c95ba3cb877e955b284e03
IsaiahSantiago <IsaiahSantiago456@gmail.com>
2023-10-22 19:06:37 -0400

    Average calculated in py

---
+++ average.py
@@ -0,0 +1,11 @@
+sum = 0
+userInput = input("Number please....")
+userInput = input("Number please....")
+userInput = input("Number please....")
+userInput = input("Number please....")
+sum = int(sum) + int(userInput)
+
+average = sum / 4
+print("The average is" + " " + str(average) )
+
+
